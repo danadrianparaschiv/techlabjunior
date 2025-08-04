8bdc4ee1325857c24d9ef84d761a39043723330d
Dan <46848558+danadrianparaschiv@users.noreply.github.com>
2025-08-04 14:16:17 +0300

    Create primul_joc_v2.py

---
+++ Programare_simpla_cu_Python/Capitolul1/primul_joc_v2.py
@@ -0,0 +1,11 @@
+print("Salut, aventurierule!")
+
+nume = input("Cum te cheamă, aventurierule? ")
+print("Bine ai venit în lumea magică, " + nume + "!")
+
+raspuns = input("Vrei să începi aventura? (da/nu) ")
+
+if raspuns == "da":
+    print("Minunat! O să fie o aventură de neuitat!")
+else:
+    print("Poate data viitoare!")
